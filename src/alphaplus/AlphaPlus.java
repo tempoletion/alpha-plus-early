657a9c36c7daa76d73f9e29d449ca306e3afcd97
tempoletion <71591108+tempoletion@users.noreply.github.com>
2023-02-02 18:57:13 +0700

    Add files via upload

--- src/alphaplus/AlphaPlus.java
+++ src/alphaplus/AlphaPlus.java
@@ -29,22 +29,12 @@ public class AlphaPlus extends Mod{
 	
 	    public AlphaPlus(){
         Log.info("Loaded Alpha-Plus mod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
         });
     }
 
     @Override
     public void loadContent(){
+    	new AlphaplusBlocks().load();
         Log.info("Loading some example content.");
     }
 

c1f468b5bfd4e51a36ac25f1fe5f590cd6f82de9
tempoletion <71591108+tempoletion@users.noreply.github.com>
2023-02-02 18:39:31 +0700

    Add files via upload

---
+++ src/alphaplus/AlphaPlus.java
@@ -0,0 +1,51 @@
+package alphaplus;
+
+import arc.*;
+import arc.assets.*;
+import arc.assets.loaders.*;
+import arc.audio.*;
+import arc.files.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.core.*;
+import mindustry.ctype.*;
+import mindustry.game.EventType.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.maps.*;
+import mindustry.mod.*;
+import mindustry.net.*;
+import mindustry.ui.*;
+import alphaplus.conteny.*;
+
+import static arc.Core.*;
+import static mindustry.Vars.*;
+
+public class AlphaPlus extends Mod{
+	
+	    public AlphaPlus(){
+        Log.info("Loaded Alpha-Plus mod constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
